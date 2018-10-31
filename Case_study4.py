e6043486d580a8eb18d23e1e1a7e9f821617bdee
MAXIMZOLOTYKH11 <max.zl@mail.ru>
2018-10-31 22:34:49 +0700

    initial commit2

--- Case_study4.py
+++ Case_study4.py
@@ -1,1 +1,39 @@
-# Case_study4
+"""Case-study #4 Анализ текста
+Разработчики:
+Иванов А.С., Петров П.С., Сидоров C.H.
+
+"""
+
+text = input("Введите текст:")
+
+count_sentens = text.count('.')
+print('Предложений:', count_sentens)
+
+count_words = text.count(' ')
+print('Слов:', count_words + 1)
+
+count_syllables = 0
+for i in text:
+    letter = i.lower()
+    if letter == "а" or letter == "о" or \
+       letter == "е" or letter == "и" or \
+       letter == "у" or letter == "ы" or \
+       letter == "ё":
+        count_syllables += 1
+print('Слогов:', count_syllables)
+
+ASL = (count_words + 1) / count_sentens
+print('Средняя длина предложения в словах:', ASL)
+ASW = count_syllables / (count_words + 1)
+print('Средняя длина слова в слогах:', ASW)
+FRE = 206.835 - (1.3 * ASL) - (60.1 * ASW)
+print('Индекс удобочитаемости Флеша:', FRE)
+
+if FRE <= 25:
+    print('Текст трудно читается (для выпускников ВУЗов).')
+elif 25 < FRE <= 50:
+    print('Текст немного трудно читать (для студентов).')
+elif 50 < FRE <= 80:
+    print('Простой текст (для школьников).')
+elif 80 < FRE:
+    print('Текст очень легко читается (для младших школьников).')
